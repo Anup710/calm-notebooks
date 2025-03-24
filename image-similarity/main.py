0653d43c729ad3015a053f3bdee5edb0ebea499f
Anup Kulkarni <anupkulkarni945@gmail.com>
2025-03-24 21:53:27 +0530

    basic deepface image check done

--- image-similarity/main.py
+++ image-similarity/main.py
@@ -1,7 +1,9 @@
-# couldnt run because of ssl error HTTPSConnectionPool(host='github.com', port=443):
-# Max retries exceeded with url: /serengil/deepface_models/releases/download/v1.0/vgg_face_weights.h5 
+# wont work on work-laptop due to persistent ssl error. 
 
 import os
+
+# to suppress flops rounfing error warnings
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = '0'
 
 #local model path
 path = "model/vgg_face_weights.h5"
@@ -9,12 +11,12 @@ path = "model/vgg_face_weights.h5"
 
 from deepface import DeepFace
 
-im1 = 'images/kiernan_shipka.png'
-im2 = 'images/mckenna_grace.png'
-
-os.environ["DEEPFACE_HOME"] = "model/vgg_face_weights.h5"
+im1 = 'images/shipka.png'
+im2 = 'images/grace.png'
 
 result = DeepFace.verify(im1, im2, model_name="VGG-Face")
+
+print(result)
 
 # print(im1)
 # result = DeepFace.verify(im1, im2)
