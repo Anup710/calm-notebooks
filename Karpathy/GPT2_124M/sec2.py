fc2a7b6d0a086de51816405e009fccb14b6f844b
Anup K <anupkulkarni945@gmail.com>
2025-10-28 15:44:19 +0530

    set hyperparams from gpt3 paper: grad clipping, cosine decay in lr with warmup, weight decay in 2D tensors + fused kernels for AdamW

--- Karpathy/GPT2_124M/sec2.py
+++ Karpathy/GPT2_124M/sec2.py
@@ -7,6 +7,7 @@ import torch.nn as nn
 from torch.nn import functional as F
 import tiktoken
 import time
+import inspect
 
 def get_device():
     # attempt to autodetect the device
@@ -161,6 +162,40 @@ class GPT(nn.Module):
             loss = F.cross_entropy(logits.view(B*T, -1), targets.view(-1))
 
         return logits, loss
+
+    
+    def configure_optimizers(self, weight_decay, learning_rate, device):
+        """
+        normally, weight decay is implemented for 2D tensors:  matmuls and embeddings (not position embd tho)
+        and not on layernorms or biases. 
+        This function separates decay and nondecay params for transparency + fuses kernels to reduce gpu
+        overheads if device = "cuda"
+        """
+        # start with all of the candidate parameters (that require grad)
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
+        
+        # create optim groups. Any parameters that is 2D will be weight decayed, otherwise no.
+        # i.e. all weight tensors in matmuls + embeddings decay, all biases and layernorms don't.
+        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+        optim_groups = [
+            {'params': decay_params, 'weight_decay': weight_decay},
+            {'params': nodecay_params, 'weight_decay': 0.0}
+        ]
+
+        num_decay_params = sum(p.numel() for p in decay_params)
+        num_nodecay_params = sum(p.numel() for p in nodecay_params)
+        print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
+        print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
+
+        # Create AdamW optimizer and use the fused version if it is available
+        fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        use_fused = fused_available and 'cuda' in device
+        print(f"using fused AdamW: {use_fused}")
+        optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=(0.9, 0.95), eps=1e-8, fused=use_fused)
+        return optimizer
+
 
     # copied as is from andrej's nb 
     @classmethod
